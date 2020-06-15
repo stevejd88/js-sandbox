9e43179a2cfd8a6706d350e475ae5a3b33e0ca00
Steven De Avila <stevejd88@gmail.com>
2020-06-15 14:02:11 -0500

    added counter const and func to keep track of game

--- door-game/index.js
+++ door-game/index.js
@@ -1,30 +1,61 @@
 const doors = document.querySelector(".doors");
 const allDoors = document.querySelectorAll(".door");
-const overlay = document.querySelector(".overlay");
-const allOverlays = document.querySelectorAll(".overlay");
+const update = document.querySelector("p");
+const refresh = document.querySelector(".refresh");
+let count = 3;
+let won = false;
 
 const found = new Audio("./sounds/take-a-picture.mp3");
 const laugh = new Audio("./sounds/laugh.mp3");
 
 const n = Math.floor(Math.random() * allDoors.length);
-console.log(n);
 
+// load a picture behind a random door
 allDoors[n].classList.add("random-peewee");
 
+// Remove or add a class
+function removeOrAdd(event, remove, add) {
+  event.target.classList.remove(remove);
+  event.target.classList.add(add);
+}
+
+// Event listener on door click
 doors.addEventListener("click", function (e) {
-  if (e.target.parentNode.classList[0] === "door") {
-    e.target.classList.remove("overlay");
-    e.target.classList.add("undisplay");
-    if (e.target.parentNode.classList[1] === "random-peewee") {
-      e.target.classList.remove("overlay");
-      e.target.classList.add("undisplay");
-      found.play();
-    } else {
-      laugh.play();
-      setTimeout(() => {
-        e.target.classList.add("overlay");
-        e.target.classList.remove("undisplay");
-      }, 1000);
+  if (won === false && count > 0) {
+    if (e.target.parentNode.classList[0] === "door") {
+      removeOrAdd(e, "overlay", "undisplay");
+      if (e.target.parentNode.classList[1] === "random-peewee") {
+        won = true;
+        removeOrAdd(e, "overlay", "undisplay");
+        found.play();
+      } else {
+        laugh.play();
+        setTimeout(() => {
+          removeOrAdd(e, "undisplay", "overlay");
+        }, 1000);
+        count -= 1;
+      }
     }
+    tryCounter(e);
   }
+});
+
+// Counter to keep track of tries
+function tryCounter(e) {
+  if (count === 2 && won === false) {
+    update.innerText = "2 tries left!";
+  } else if (count === 1 && won === false) {
+    update.innerText = "1 Try left!";
+  } else if (count === 0 && won === false) {
+    update.innerText = "Better luck Next Time!";
+    refresh.style.display = "block";
+  } else if (won === true) {
+    update.innerText = "You found him!!!";
+    refresh.style.display = "block";
+  }
+}
+
+// Event listener to reload game
+refresh.addEventListener("click", function () {
+  location.reload();
 });
