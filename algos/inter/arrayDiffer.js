8d83a630414eb8eadbb04cef9972033c523f6d66
Steven De Avila <stevejd88@gmail.com>
2021-05-10 09:41:10 -0500

    Add differing arrays algorithm

--- algos/inter/arrayDiffer.js
+++ algos/inter/arrayDiffer.js
@@ -2,42 +2,19 @@
 // the two given arrays, but not both. In other words, return the symmetric
 //  difference of the two arrays.
 
-// function diffArray(arr1, arr2) {
-//   let newArr = [],
-//     first,
-//     second;
-
-//   if (arr1.length > arr2.length) {
-//     first = arr1;
-//     second = arr2;
-//   } else {
-//     first = arr2;
-//     second = arr1;
-//   }
-
-//   for (let i = 0; i < first.length; i++) {
-//     for (let j = 0; j < second.length; j++) {
-//       if (!first.includes(second[j]) && !newArr.includes(second[j])) {
-//         newArr.push(second[j]);
-//         console.log(newArr);
-//       } else if (!second.includes(first[i]) && !newArr.includes(first[i])) {
-//         newArr.push(first[i]);
-//         console.log(newArr);
-//       }
-//     }
-//   }
-//   return newArr;
-// }
-
 function diffArray(arr1, arr2) {
-  const array = arr1.concat(arr2);
   let newArr = [];
 
-  array.forEach((item) => {
-    if (!newArr.includes(item)) {
-      newArr.push(item);
-    }
-  });
+  function filterArr(arrOne, arrTwo) {
+    arrOne.forEach((number) => {
+      if (!arrTwo.includes(number)) {
+        newArr.push(number);
+      }
+    });
+  }
+
+  filterArr(arr1, arr2);
+  filterArr(arr2, arr1);
 
   return newArr;
 }
@@ -47,3 +24,14 @@ const output = diffArray(
   ['diorite', 'andesite', 'grass', 'dirt', 'dead shrub']
 );
 console.log(output);
+
+// ============================================================
+// ===================== ANOTHER WAY ==========================
+// ============================================================
+// function diffArray(arr1, arr2) {
+//   return [...diff(arr1, arr2), ...diff(arr2, arr1)];
+
+//   function diff(a, b) {
+//     return a.filter(item => b.indexOf(item) === -1);
+//   }
+// }
